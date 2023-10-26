ac6e4e3cb58748c240f4bd106298d19855c78ed4
Dustin Yochim <yochim.dustin@gmail.com>
2023-10-26 10:30:18 -0500

    Add ship factory function

--- src/js/modules/ship.js
+++ src/js/modules/ship.js
@@ -1,5 +1,12 @@
 export default function ship(length) {
   return {
     length,
+    numHits: 0,
+    isSunk() {
+      return this.numHits === this.length;
+    },
+    hit() {
+      this.numHits++;
+    },
   };
 }
--- test/ship.test.js
+++ test/ship.test.js
@@ -4,3 +4,29 @@ test("ship.length correctly returns ships size", () => {
   const carrier = ship(5);
   expect(carrier.length).toBe(5);
 });
+
+test("ship.numHits correctly returns ships number of hits", () => {
+  const carrier = ship(5);
+  expect(carrier.numHits).toBe(0);
+  carrier.hit();
+  expect(carrier.numHits).toBe(1);
+  carrier.hit();
+  expect(carrier.numHits).toBe(2);
+  carrier.hit();
+  expect(carrier.numHits).toBe(3);
+});
+
+test("ship.isSunk correctly returns ships sunk status", () => {
+  const carrier = ship(5);
+  expect(carrier.isSunk()).toBe(false);
+  carrier.hit();
+  expect(carrier.isSunk()).toBe(false);
+  carrier.hit();
+  expect(carrier.isSunk()).toBe(false);
+  carrier.hit();
+  expect(carrier.isSunk()).toBe(false);
+  carrier.hit();
+  expect(carrier.isSunk()).toBe(false);
+  carrier.hit();
+  expect(carrier.isSunk()).toBe(true);
+});
