e4fd8f649be2022a6886e04d5290193da4789775
Dustin Yochim <yochim.dustin@gmail.com>
2024-01-10 21:11:24 -0600

    feat(users): add gameboard

--- src/js/modules/Gameboard.js
+++ src/js/modules/Gameboard.js
@@ -6,25 +6,34 @@ class Gameboard {
   }
 
   createBoard() {
-    const board = new Array(this.rows * this.cols).fill(null).map((_, i) => ({
-      index: i,
-      cell: this.indexToCell(i),
-      ship: null,
-      isHit: false,
-    }));
-    return board;
+    const gameBoard = [];
+    for (let i = 0; i < this.rows; i++) {
+      gameBoard[i] = [];
+      for (let j = 0; j < this.cols; j++) {
+        gameBoard[i][j] = {
+          row: String.fromCharCode(i + 65),
+          col: j,
+          index: 10 * i + j,
+          cell: `${String.fromCharCode(i + 65)}${j + 1}`,
+          status: null,
+        };
+      }
+    }
+    return gameBoard;
   }
 
-  indexToCell(index) {
-    const row = Math.floor(index / this.cols) + 1;
-    const col = String.fromCharCode(65 + (index % this.cols));
-    return `${row}${col}`;
-  }
+  placeShip(ship, index, orientation) {
+    // Check valid index
+    if (index < 0 || index > 99) {
+      console.error("Error");
+      return;
+    }
 
-  cellToIndex(cell) {
-    const row = parseInt(position.slice(0, -1)) - 1;
-    const col = position.slice(-1).charCodeAt(0) - 65;
-    return row * this.cols + col;
+    let len = ship.length;
+
+    // Horizontal Placement
+
+    // Vertical Placement
   }
 }
 
--- test/Gameboard.test.js
+++ test/Gameboard.test.js
@@ -1,1 +1,24 @@
-import Ship from "../src/js/modules/Ship.js";
+import Gameboard from "../src/js/modules/gameboard";
+import Ship from "../src/js/modules/Ship";
+
+describe("Gameboard", () => {
+  let gameboard;
+
+  beforeAll(() => {
+    /* Runs before all tests */
+    gameboard = new Gameboard();
+  });
+  afterAll(() => {
+    /* Runs after all tests */
+  });
+  beforeEach(() => {
+    /* Runs before each test */
+  });
+  afterEach(() => {
+    /* Runs after each test */
+  });
+
+  test("gameboard has 100 cells", () => {
+    expect(gameboard.board.length).toEqual(10);
+  });
+});
